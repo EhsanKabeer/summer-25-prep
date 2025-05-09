cf1c3e61d657c544e730fff0d24e098e7305d49b
Ehsan <eskabeer@umich.edu>
2025-05-09 17:05:40 -0400

    Completed Merge two Lists

--- MergeTwoSortedLists.cpp
+++ MergeTwoSortedLists.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <list>
 #include <string>
 using namespace std;
 
@@ -20,7 +21,47 @@ class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         ListNode* newHead = nullptr;
+        if (list1 == nullptr && list2 == nullptr) {
+            return newHead;
+        }
+        if (list1 == nullptr && list2 != nullptr) {
+            return list2;
+        }
+        if (list2 == nullptr && list1 != nullptr){
+            return list1;
+        }
 
+        if (list1->val >= list2->val) {
+            newHead = list2;
+            list2 = list2->next;
+        }
+        else {
+            newHead = list1;
+            list1 = list1->next;
+        }
+        ListNode* prev = nullptr;
+        auto curr = newHead;
+        while (list1 != nullptr && list2 != nullptr) {
+            if (list1->val >= list2->val) {
+                curr->next = list2;
+                prev = curr;
+                curr = curr->next;
+                list2 = list2->next;
+            }
+            else {
+                curr->next = list1;
+                prev = curr;
+                curr = curr->next;
+                list1 = list1->next;
+            }
+        }
+        if (!list1) {
+            curr->next = list2;
+        }
+        else if (!list2) {
+            curr->next = list1;
+        }
+        return newHead;
 
     }
 };
@@ -42,6 +83,7 @@ int main() {
 
     Solution test;
     auto mergedHead = test.mergeTwoLists(head1,head2);
+
     while (mergedHead) {
         cout << mergedHead << ' ';
     }
